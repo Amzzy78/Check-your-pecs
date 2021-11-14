9073f4b172d38b204c2cf25e67fa6bfcc4bc9450
Stuart Raynor <madstu@gmail.com>
2021-11-14 22:58:09 +0000

    Add signup confirmation within modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
  * Add or remove "responsive" from the class name
  */
 function hamburgerLinks() {
-    const x = document.getElementById("myTopnav");
-    (x.className === "topnav") ? x.className += " responsive" : x.className = "topnav";
+  const x = document.getElementById("myTopnav");
+  (x.className === "topnav") ? x.className += " responsive" : x.className = "topnav";
 }
 
 // Get the modal
@@ -11,9 +11,9 @@ const modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
 }
 
 //Get the button
@@ -35,3 +35,54 @@ function goTopFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+/**
+ * Close the modal
+ */
+function closeModal(){
+  document.getElementById('id01').style.display='none';
+}
+
+// Handle form data
+function formSubmit() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  //Check they've filled their name and email
+  if (name == "" || email == "") {
+    return false;
+  }
+
+  // Get date and extract day of month
+  const d = new Date();
+  let da=d.getDate();
+  
+  if (da > 28){
+    da=1;
+  }
+
+  //Add the ordinal
+  const day = dateOrdinal(da);
+  
+  //Change the HTML
+  document.getElementById("form-area").innerHTML = `
+      <br><br>
+      <h1>Thank You, ${name}!</h1>
+      <p>We'll send an email to ${email} every month on the ${day}</p>
+      <br><br>
+
+      <div class="clearfix">
+          <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Close</button>
+      </div>
+      <br><br>
+  `;
+}
+
+//Below credit: https://stackoverflow.com/questions/15397372/javascript-new-date-ordinal-st-nd-rd-th/15397495
+
+/**
+ * Returns date of month with correct st, nd, rd or th
+ */
+function dateOrdinal(d) {
+  return d+(31==d||21==d||1==d?"st":22==d||2==d?"nd":23==d||3==d?"rd":"th")
+};
